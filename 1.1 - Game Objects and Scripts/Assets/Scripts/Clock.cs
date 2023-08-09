93a616bd67b94655f6810b5884b5028cd6f121af
Hans Ludvig Flått-Bjørnstad <space@hludde.no>
2023-08-09 23:11:30 +0200

    Updated 1.1 Basics - Game Objects and Scripts

--- 1.1 - Game Objects and Scripts/Assets/Scripts/Clock.cs
+++ 1.1 - Game Objects and Scripts/Assets/Scripts/Clock.cs
@@ -1,33 +1,17 @@
 ﻿using System;
 using UnityEngine;
-public class Clock : MonoBehaviour{
 
-    const float degreesPerHour = 30f;
-    const float degreesPerMinute = 6f;
-    const float degreesPerSeconds = 6f;
+public class Clock : MonoBehaviour
+{
+    const float hoursToDegrees = -30f, minutesToDegrees = -6f, secondsToDegrees = -6f;
+    
+    [SerializeField]
+    Transform hoursPivot, minutesPivot, secondsPivot;
 
-    public Transform hoursTransform, minutesTransform, secondsTransform;
-    public bool continuous;
-
-    void Update(){
-        if(continuous){
-            UpdateContinuous();
-        }else{
-            UpdateDiscrete();
-        }
-    }
-
-    void UpdateContinuous() {
+    private void Update() {
         TimeSpan time = DateTime.Now.TimeOfDay;
-        hoursTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalHours * degreesPerHour, 0f);
-        minutesTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalMinutes * degreesPerMinute, 0f);
-        secondsTransform.localRotation = Quaternion.Euler(0f, (float)time.TotalSeconds * degreesPerSeconds, 0f);
-    }
-
-    void UpdateDiscrete() {
-        DateTime time = DateTime.Now;
-        hoursTransform.localRotation = Quaternion.Euler(0f, time.Hour * degreesPerHour, 0f);
-        minutesTransform.localRotation = Quaternion.Euler(0f, time.Minute * degreesPerMinute, 0f);
-        secondsTransform.localRotation = Quaternion.Euler(0f, time.Second * degreesPerSeconds, 0f);
+        hoursPivot.localRotation = Quaternion.Euler(0f, 0f, hoursToDegrees * (float) time.TotalHours);
+        minutesPivot.localRotation = Quaternion.Euler(0f, 0f, minutesToDegrees * (float) time.TotalMinutes);
+        secondsPivot.localRotation = Quaternion.Euler(0f, 0f, secondsToDegrees * (float) time.TotalSeconds);
     }
 }
