ba0ea2656247e1799de2dbe90f3ae18fb50933ce
Hans Ludvig Flått-Bjørnstad <space@hludde.no>
2023-08-10 20:58:50 +0200

    Updated 1.2 Basics - Building a Graph

--- 1.2 - Building a Graph/Assets/Scripts/Graph.cs
+++ 1.2 - Building a Graph/Assets/Scripts/Graph.cs
@@ -1,36 +1,46 @@
-﻿using UnityEngine;
-public class Graph : MonoBehaviour{
-    public Transform pointPrefab;
-    [Range (10,100)] public int Resolution = 10;
+﻿using System;
+using UnityEngine;
 
+public class Graph : MonoBehaviour
+{
+    [SerializeField]
+    Transform pointPrefab;
+    
+    [SerializeField, Range(10, 100)]
+    int resolution = 10;
+    
     Transform[] points;
+    
+    void Awake()
+    {
+        float step = 2f / resolution;
+        var position = Vector3.zero;
+        var scale = Vector3.one * step;
 
-    void Awake(){
-        float step = 2f / Resolution;
-        Vector3 scale = Vector3.one * step;
-        Vector3 position;
-
-        position.y = 0f;
-        position.z = 0f;
-        points = new Transform[Resolution];
-
-        for(int i = 0; i<points.Length; i++){
-            Transform point = Instantiate(pointPrefab);
-            position.x = (i + 0.5f) * step - 1f;
+        points = new Transform[resolution];
+        for(int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i] = Instantiate(pointPrefab);
+            
+            position.x = ((i + 0.5f) * step - 1f);
+            
             point.localPosition = position;
             point.localScale = scale;
-            points[i] = point;
             point.SetParent(transform, false);
         }
     }
 
-    void Update(){
-        for(int i=0; i<points.Length; i++){
+    void Update()
+    {
+        float time = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
             Transform point = points[i];
             Vector3 position = point.localPosition;
-            position.y = Mathf.Sin(Mathf.PI*(position.x+Time.time));
+
+            position.y = Mathf.Sin(Mathf.PI * (position.x + time));
+            
             point.localPosition = position;
         }
     }
-
 }
